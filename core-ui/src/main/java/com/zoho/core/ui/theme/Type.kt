ec587ad6852404491c9b82cf415c7df02ae62d9e
Karthik Sankar <karthiksankar.dev@gmail.com>
2022-10-27 21:34:05 +0530

    feat: add custom puvi font

--- core-ui/src/main/java/com/zoho/core/ui/theme/Type.kt
+++ core-ui/src/main/java/com/zoho/core/ui/theme/Type.kt
@@ -2,33 +2,123 @@ package com.zoho.core.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.zoho.core.ui.R
 
-// Set of Material typography styles to start with
+private val PuviFontFamily = FontFamily(
+    Font(R.font.puvi_regular, weight = FontWeight.Normal, style = FontStyle.Normal),
+    Font(R.font.puvi_regular_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.puvi_medium, weight = FontWeight.Medium, style = FontStyle.Normal),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = PuviFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.0.sp,
+        letterSpacing = 0.2.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.0.sp,
+        letterSpacing = 0.4.sp,
+    ),
+    displayLarge = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 57.sp,
+        lineHeight = 64.0.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.0.sp,
+        letterSpacing = 0.0.sp,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 44.0.sp,
+        letterSpacing = 0.0.sp,
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.0.sp,
+        letterSpacing = 0.0.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.0.sp,
+        letterSpacing = 0.0.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.0.sp,
+        letterSpacing = 0.0.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.0.sp,
+        letterSpacing = 0.1.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.0.sp,
+        letterSpacing = 0.5.sp,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = PuviFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleMedium = TextStyle(
+        fontFamily = PuviFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 16.sp,
+        lineHeight = 24.0.sp,
+        letterSpacing = 0.2.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = PuviFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.0.sp,
+        letterSpacing = 0.1.sp,
     )
-    */
 )
